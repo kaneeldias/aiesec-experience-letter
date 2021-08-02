e6ca00f68d8f91c172a7e69af6be4dddc1b4edbe
kaneeldias@hotmail.com <kaneeldias@hotmail.com>
2021-08-03 03:16:13 +0530

    generate google doc

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,21 @@
 const functions = require("firebase-functions");
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
-
 let test = require("./test.js");
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore();
 
 exports.onNewRequest = functions.firestore.document('/requests/{documentId}')
-  .onCreate((snap, context) => {
-
+  .onCreate( async (snap, context) => {
+    let docUrl = await test.test(snap.data());
+    console.log(docUrl);
+    let email = {
+      subject: snap.data().name + " has requested for an experience letter.",
+      data: snap.data(),
+      mcvp_email: 'kaneel.dias2@aiesec.net',
+      member_email: snap.data().email,
+      docUrl: docUrl
+    }
+    await db.collection("emails").add(email)
   });
 
 exports.test = functions.https.onRequest(async (req, res) => {
--- functions/test.js
+++ functions/test.js
@@ -1,15 +1,15 @@
 const googleDrive = require('@googleapis/drive')
 const googleDocs = require('@googleapis/docs')
 const {GoogleAuth} = require('google-auth-library');
+const dateFormat = require('dateformat');
 
 module.exports = {
-  test: async function () {
+  test: async function (data) {
 
     const auth = new GoogleAuth({
       keyFilename: 'credentials.json',
       scopes: ['https://www.googleapis.com/auth/documents', 'https://www.googleapis.com/auth/drive']
     });
-    const authClient = await auth.getClient();
 
     const driveClient = await googleDrive.drive({
       version: "v2",
@@ -22,14 +22,7 @@ module.exports = {
       auth: auth
     });
 
-    let requests = [
-      {
-        replaceAllText: {
-          containsText: {text: '{{name}}'},
-          replaceText: "Kaneel Dias",
-        },
-      }
-    ];
+    let requests = getRequestData(data);
 
     let res = await docsClient.documents.batchUpdate({
       documentId: copyDocumentId,
@@ -51,4 +44,76 @@ async function copyTemplate(driveClient, docName) {
     resource: request,
   });
   return response.data.id;
+}
+
+function getRequestData(data) {
+  let requests = [];
+
+  //Date
+  let date = dateFormat(new Date(), "mmmm dS, yyyy");
+  requests.push(replaceTemplate("date", date));
+
+  //Name
+  requests.push(replaceTemplate("name", data.name));
+
+  //First Name
+  requests.push(replaceTemplate("first_name", data.name.split(" ")[0]));
+
+  //Joined
+  let joined = dateFormat(data.joined.toDate(), "mmmm dS, yyyy");
+  requests.push(replaceTemplate("joined", joined));
+
+  //Exit
+  let exit;
+  if (data.exit == null) exit = dateFormat(new Date(), "mmmm dS, yyyy");
+  else exit = dateFormat(data.exit.toDate(), "mmmm dS, yyyy");
+  requests.push(replaceTemplate("exit", exit));
+
+  //Positions
+  let positions = "";
+  for (let position of data.positions) {
+    if (position.role == null) continue;
+    positions += "\t•    " + position.role + " for " + position.function + " of " + position.entity + "\n";
+  }
+  requests.push(replaceTemplateRaw("{{positions}}\n", positions));
+
+  //Conferences
+  let conferences = "";
+  for (let conference of data.conferences) {
+    if (conference.role == null) continue;
+    conferences += "\t•    " + conference.role + " - " + conference.name + " " + conference.year + "\n";
+  }
+  if (conferences === "") {
+    requests.push(replaceTemplateRaw(
+      "\nFurther they got the opportunity to take part in following Conferences & Events.:\n", ""));
+  }
+  requests.push(replaceTemplateRaw("{{conferences}}\n", conferences));
+
+  //Achievements
+  let achievements = "";
+  for (let achievement of data.achievements) {
+    if (achievement.title == null) continue;
+    achievements += "\t•    " + achievement.title + " - " + achievement.entity + " " + achievement.term + "\n";
+  }
+  if (achievements === "") {
+    requests.push(replaceTemplateRaw(
+      "\nFollowing are the commendable achievements of " + data.name + " as a Leader in AIESEC.\n", ""));
+  }
+  requests.push(replaceTemplateRaw("{{achievements}}\n", achievements));
+
+  return requests;
+
+}
+
+function replaceTemplate(search, replace) {
+  return replaceTemplateRaw('{{'+search+'}}', replace)
+}
+
+function replaceTemplateRaw(search, replace) {
+  return {
+    replaceAllText: {
+      containsText: {text: search},
+      replaceText: replace
+    }
+  }
 }
