946a061397e4383516baed67c0d84018f09f2043
kaneeldias <kaneeldias@hotmail.com>
2021-08-06 20:13:32 +0530

    Clean up code

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,9 @@
 const functions = require("firebase-functions");
-let test = require("./test.js");
-const admin = require('firebase-admin');
-admin.initializeApp();
-const db = admin.firestore();
+let docs = require("./docs.js");
+let email = require("./email.js");
 
 exports.onNewRequest = functions.firestore.document('/requests/{documentId}')
   .onCreate( async (snap, context) => {
-    let docUrl = await test.test(snap.data());
-
-    const emailConfig = (await db.collection('config').doc('email').get()).data();
-
-    let email = {
-      to: emailConfig.lcvps_pm[snap.data().entity],
-      cc: [emailConfig.mcvp_im],
-      replyTo: emailConfig.mcvp_tm,
-      message: {
-        subject: snap.data().name + " has requested for an experience letter.",
-        text: docUrl
-      },
-      data: snap.data(),
-    }
-    await db.collection("emails").add(email)
+    let docUrl = await docs.create(snap.data());
+    await email.create(snap.data(), docUrl);
   });
